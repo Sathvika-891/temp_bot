5eb38eb63595b00875d431b710036f39d2fdace0
Sathvika-891 <sathvikakopalli@gmail.com>
2025-05-26 18:20:47 +0530

    added fastapi

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from fastapi import  FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import StreamingResponse, FileResponse
-from src.chat2 import Chatbot
+from fastapi.responses import StreamingResponse
+from src.chatbot import Chatbot
+from src.models import ChatbotUserInput
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*']
 )
 chat_session=Chatbot()
+
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from datetime import datetime
+
 @app.get("/")
 def root():
     return {"welcome":"home"}
-@app.get("/chat_stream/{message}")
-def chat_stream_events(message: str):
-    print("Received query:",message)
-    return StreamingResponse(chat_session.generate_response(query=message), media_type="text/event-stream")
+@app.post("/chat_stream")
+def chat_stream_events(chat_input: ChatbotUserInput):
+    print("Received query:",chat_input.query)
+    return StreamingResponse(chat_session.generate_response(chat_input), media_type="text/event-stream")
 
 if __name__=="__main__":
     import uvicorn
